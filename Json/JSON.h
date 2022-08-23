ed084e98c00a207801206e3a120327508d8b5262
chggu <chggu2@126.com>
2022-08-23 21:28:33 +0800

    开发中

---
+++ Json/JSON.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef JSON_H__
+#define JSON_H__
+#include<stdio.h>
+#endif
+typedef enum{T_NULL,T_FALSE,T_TRUE,T_NUMBER};//T_ TYPE
--- Json/Main.cpp
+++ Json/Main.cpp
@@ -1,8 +1,15 @@
-/*
-* Ö÷º¯Êý
+ï»¿/*
+* ä¸»å‡½æ•°
 * Copyright(C) 2022 chggu
 */
-#ifndef JSON_MAIN_H__
-#define JSON_MAIN_H__
+#ifndef JSON_H__
+#define JSON_H__
 #include<stdio.h>
 #endif
+#include"JSON.h"
+static int main_RTVal = 0;
+
+int main(int argc, char* argv[]) {
+
+	return main_RTVal;
+}
