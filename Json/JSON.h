abf697a23c35b74c9304aff04c2570daf3c1d796
chggu <chggu2@126.com>
2022-12-20 00:23:12 +0800

    new

--- Json/JSON.h
+++ Json/JSON.h
@@ -1,6 +1,14 @@
-#pragma once
-#ifndef JSON_H__
-#define JSON_H__
-#include<stdio.h>
-#endif
-typedef enum{T_NULL,T_FALSE,T_TRUE,T_NUMBER};//T_ TYPE
+#ifndef CH_JSON_H__
+	#define CH_JSON_H__
+	typedef enum { J_NULL, J_FALSE, J_TRUE, J_NUMBER, J_OBJECT, J_STRING, J_ARRAY } j_type;
+	typedef struct {
+		j_type type;
+	}j_value;
+	int j_parse(j_value* v, const char* json);
+	enum j_parse_ret {
+		J_PARSE_OK = 0,
+		J_PARSE_EXPECT_VALUE,
+		J_PARSE_INVALID_VALUE,
+		J_PARSE_ROOT_NOT_SINGULAR
+	};
+#endif // !CH_JSON_H__
